7bc3e2fc95817030cbbf2cd34cfc01fb1629d522
Robot <robot@neckar.it>
2025-07-01 00:42:22 +0000

    publishing updated develop sources

--- buildSrc/src/main/kotlin/Tools.kt
+++ buildSrc/src/main/kotlin/Tools.kt
@@ -22,22 +22,7 @@ val Project.tools: File
 @Deprecated("use JibCliPlugin instead")
 val Project.jibCli: File
   get() {
-    val jibCliDir = File(tools, "jib-cli/jib-0.13.0")
-    val jibCliBinDir = File(jibCliDir, "bin").also { require(it.isDirectory) { "jib-cli bin directory <${it.absolutePath}> does not exist or is not a directory" } }
-
-    return when {
-      SystemUtils.IS_OS_LINUX -> {
-        File(jibCliBinDir, "jib").also { require(it.isFile) { "jib-cli binary <${it.absolutePath}> does not exist or is not a file" } }
-      }
-
-      SystemUtils.IS_OS_WINDOWS -> {
-        File(jibCliBinDir, "jib.bat").also { require(it.isFile) { "jib-cli binary <${it.absolutePath}> does not exist or is not a file" } }
-      }
-
-      else -> {
-        throw UnsupportedOperationException("unsupported OS detected")
-      }
-    }
+    throw UnsupportedOperationException("Use JibCliPlugin instead")
   }
 
 @Deprecated("Use the installed docker compose", level = DeprecationLevel.ERROR)
