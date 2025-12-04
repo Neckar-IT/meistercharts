e9013eac017d982b6f00c0dc5cf9d1c9cfc5f5e8
Robot <robot@neckar.it>
2025-12-04 03:27:13 +0000

    publishing updated develop sources

--- meistercharts-commons/src/commonMain/kotlin/it/neckar/open/time/TimeMillisExample.kt
+++ meistercharts-commons/src/commonMain/kotlin/it/neckar/open/time/TimeMillisExample.kt
@@ -33,6 +33,10 @@ object TimeMillisExample {
     requireMatchesFormattedUtc(it, "2026-04-24T03:10:00.000Z")
   }
 
+  val _2026_09_30_17_38_12_123Z: @ms Double = 1.790789892E12.also {
+    requireMatchesFormattedUtc(it, "2026-09-30T17:38:12.000Z")
+  }
+
   init {
     require(_2020_05_21_15_00_41_500.formatUtcForDebug() == "2020-05-21T15:00:41.500Z")
     require(_2019_01_07_15_45_19_123_987_345_678_901.formatUtcForDebug() == "2019-01-07T15:45:19.123Z")
@@ -41,8 +45,8 @@ object TimeMillisExample {
     require(_2020_05_21_15_00_41_500Z.formatUtcForDebug() == "2020-05-21T15:00:41.500Z")
     require(_2024_09_12_06_44_17_500Z.formatUtcForDebug() == "2024-09-12T06:44:17.500Z")
     require(_2026_04_24_03_10_00_000Z.formatUtcForDebug() == "2026-04-24T03:10:00.000Z")
+    require(_2026_09_30_17_38_12_123Z.formatUtcForDebug() == "2026-09-30T17:38:12.000Z")
   }
-
 
   val entries: List<@ms Double> = listOf(
     _2020_05_21_15_00_41_500,
@@ -50,9 +54,10 @@ object TimeMillisExample {
     _2022_07_29_09_38_04_141Z,
     _2019_01_07_10_11_59_123Z,
     _2020_05_21_15_00_41_500Z,
-    _2024_09_12_06_44_17_500Z
+    _2024_09_12_06_44_17_500Z,
+    _2026_04_24_03_10_00_000Z,
+    _2026_09_30_17_38_12_123Z,
   )
-
 
   /**
    * Helper method to verify the formatted UTC string
